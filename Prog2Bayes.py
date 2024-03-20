43ce6a74bee2d837af868c3cb809544acd6d6225
Umbara1077 <108435047+Umbara1077@users.noreply.github.com>
2024-03-19 20:45:28 -0400

    d

---
+++ Prog2Bayes.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+
+class NaiveBayes:
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+        self.classes = np.unique(y)
+
+    def calculate_likelihood(self, x, mean, stdev):
+        exponent = np.exp(-((x - mean) ** 2 / (2 * stdev ** 2)))
+        return (1 / (np.sqrt(2 * np.pi) * stdev)) * exponent
+
+    def calculate_class_probabilities(self, input_data):
+        probabilities = {}
+        for class_value in self.classes:
+            class_indices = np.where(self.y_train == class_value)[0]
+            class_data = self.X_train[class_indices]
+
+            # Calculate mean and standard deviation for each feature
+            class_means = np.mean(class_data, axis=0)
+            class_stdevs = np.std(class_data, axis=0)
+
+            # Calculate likelihood for each feature
+            likelihood = np.prod(self.calculate_likelihood(input_data, class_means, class_stdevs))
+
+            # Calculate prior probability
+            prior = len(class_indices) / len(self.y_train)
+
+            # Calculate class probability using Bayes' theorem
+            probabilities[class_value] = prior * likelihood
+
+        return probabilities
+
+    # Use it for further predicting model
+    def predict(self, X):
+        predictions = []
+        for sample in X:
+            probabilities = self.calculate_class_probabilities(sample)
+            predicted_class = max(probabilities, key=probabilities.get)
+            predictions.append(predicted_class)
+        return predictions
+
+# Data Frame example
+
+data = [['Blue','Blue','Brown','Yes'],['Red','Blue','Brown','Yes'],
+        ['Red','Brown','Black','No'],['Red','Green','Brown','No'],
+        ['Green','Green','Blonde','Yes'],['Blue','Brown','Blonde','No']]
+
+
+df = pd.DataFrame( data, columns = ['Favorite Color', 'Eye Color', 'Hair Color', 'Happy'] )
+
+
+features = df[['Favorite Color','Eye Color','Hair Color']].values
+target = df['Happy'].values
+
+# Initialize the Naive Bayes class
+gnb = NaiveBayes()
+
+# Use methods in class
+gnb.fit(features, target)
+
+y_pred = gnb.predict(features)
+
+accuracy = np.mean(y_pred == target)
+print("Accuracy:", accuracy)
---
+++ Prog2KNN.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+
+def euclidean_distance(x1, x2):
+    return np.sqrt(np.sum((x1 - x2)**2))
+
+class KNN:
+    def __init__(self, k=10):
+        self.k = k
+
+    def fit(self, X, Y):
+        self.X_train = X
+        self.Y_train = Y
+
+    def predict(self, X):
+        y_pred = [self._predict(x) for x in X]
+        return np.array(y_pred)
+
+    def _predict(self, x):
+        distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
+        k_indices = np.argsort(distances)[:self.k]
+        k_nearest_labels = [self.Y_train[i] for i in k_indices]
+        most_common = np.argmax(np.bincount(k_nearest_labels))
+        return most_common
+
+def train_test_split(X, y, test_size=0.2, random_state=None):
+    np.random.seed(random_state)
+    shuffled = np.random.permutation(len(X))
+    test_set_size = int(len(X) * test_size)
+    test = shuffled[:test_set_size]
+    train = shuffled[test_set_size:]
+    return X[train], X[test], y[train], y[test]
+
+# Data Frame Example
+
+data = [[2,2,0],[3,1,0],
+        [4,1,0],[1,5,0],
+        [-1,3,0],[-3,-2,0]]
+
+
+df = pd.DataFrame( data, columns = ['Test1','Test2','Find'] )
+
+
+features = df[['Test1', 'Test2']].values
+target = df['Find'].values
+
+# iterate though every point and calculate distance (norm), then take the values of the 6 smallest distances
+# a way to do that might be indexing at a slice of an argsort
+
+# Split the data into training and testing sets
+X_train, X_test, Y_train, Y_test = train_test_split(features, target, random_state=42)
+
+# Initialize KNN 
+k = 10
+knn = KNN(k=k)
+
+# Uses methods in KNN Class
+knn.fit(X_train, Y_train)
+
+y_pred = knn.predict(X_test)
+
+accuracy = np.mean(y_pred == Y_test)
+print("Accuracy:", accuracy)
